7cfe86290e6751d3764c9f294341d517d53a9c47
Chad LeJeune <chad.lejeune23@gmail.com>
2021-11-26 14:45:16 -0600

    Updated test.py formatting

--- app/app/test.py
+++ app/app/test.py
@@ -4,7 +4,7 @@ from app.calc import add
 
 
 class CalcTests(TestCase):
-    
+
     def test_add_numbers(self):
         """Test that two numbers are added together"""
         self.assertEqual(add(3, 8), 11)
